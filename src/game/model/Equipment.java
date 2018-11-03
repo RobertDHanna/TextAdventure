454d66492f8912a19ca4113b1b4cb700fd8c664a
Jared <jared.haviland@gmail.com>
2018-11-02 22:25:39 -0600

    Added base enemy and equipment classes

--- src/game/model/Equipment.java
+++ src/game/model/Equipment.java
@@ -1,4 +1,28 @@
 package game.model;
 
-public class Equipment {
+import java.util.List;
+
+public class Equipment extends Item {
+
+    public Equipment(String id, String description, Boolean pickupable, String inRoomDescription, List<String> actionTriggers) {
+        super(id, description, pickupable, inRoomDescription, actionTriggers);
+    }
+
+    public enum Bonus {
+        STR, DEX, INT, CON
+    }
+    private List<Bonus> bonuses;
+
+    public enum Slot {
+        HEAD, TORSO, ARMS, HANDS, RING, LEGS, FEET
+    }
+    private Slot slot;
+
+    List<Bonus> getBonuses() {
+        return bonuses;
+    }
+
+    Slot getSlot() {
+        return slot;
+    }
 }
