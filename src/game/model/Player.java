3c91c88becc7b9167db62e7ff189798c0e408601
Jared <jared.haviland@gmail.com>
2018-11-02 19:30:20 -0600

    Adding Player class

--- src/game/model/Player.java
+++ src/game/model/Player.java
@@ -1,5 +1,23 @@
 package game.model;
 
-public class Player
-{
+import java.util.List;
+
+public class Player {
+
+    private int max_hp;
+    private int current_hp;
+    private int max_ap;
+    private int current_ap;
+
+    private int strength; //affects how much damage you deal at a time (still need to figure out calculations)
+    private int dexterity; //affects your chance to hit enemies and the chance of enemies to hit you (still need to figure out calculations)
+    private int intelligence; //determines your max ability points, which determines how often you can use special abilities; max_ap = 2x intelligence
+    private int constitution; //determines your max health; max_hp = 4x constitution
+
+    List<Equipment> equippedItems;
+    List<Item> inventory;
+
+    void addToInventory(Item item) {
+        inventory.add(item);
+    }
 }
